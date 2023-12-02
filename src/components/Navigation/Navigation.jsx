96e4a967c23c5a9937e30fb597cce30996013e1a
iVladyuser <korol.v09@gmail.com>
2023-12-02 16:32:30 +0100

    added RestrictedRoutes and PrivateRoutes

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,15 @@
 import { Toolbar } from '@mui/material';
 import { StyledNavLink, toolbarStyle } from 'components/Navigation/StyleNav';
+import { useSelector } from 'react-redux';
+import { selectAuthenticated } from 'redux/auth/authSelectors';
 
 export const Navigation = () => {
+  const authenticated = useSelector(selectAuthenticated);
+
   return (
     <Toolbar sx={toolbarStyle}>
       <StyledNavLink to="/">Home</StyledNavLink>
-      <StyledNavLink to="/contacts">Contacts</StyledNavLink>
+      {authenticated && <StyledNavLink to="/contacts">Contacts</StyledNavLink>}
     </Toolbar>
   );
 };
---
+++ src/components/RestrictedRoute.jsx
@@ -0,0 +1,12 @@
+import { CONTACTS_ROUTE } from 'constants/routes';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { selectAuthenticated } from 'redux/auth/authSelectors';
+
+const RestrictedRoute = ({ children, navigateTo = CONTACTS_ROUTE }) => {
+  const authenticated = useSelector(selectAuthenticated);
+  return authenticated ? <Navigate to={navigateTo} replace /> : children;
+};
+
+export default RestrictedRoute;
