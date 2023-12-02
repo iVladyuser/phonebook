4ca229a51c692cb3acb620c8f96548cc74e85539
iVladyuser <129949946+iVladyuser@users.noreply.github.com>
2023-12-02 15:34:03 +0000

    Deploying to gh-pages from @ iVladyuser/goit-react-hw-08-phonebook@d957883fc2aebdcf95f85efb3fb97c22cc2a85b2 🚀

---
+++ static/js/983.d0ae16a0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{3983:function(e,n,t){t.r(n),t.d(n,{HomePage:function(){return r}});var o=t(3329),r=function(){return(0,o.jsx)("div",{style:{display:"flex",flexDirection:"column",gap:"15px",margin:"0 auto",padding:"30px"},children:(0,o.jsx)("h1",{style:{color:"#3645ab",marginTop:"50px"},children:"Phonebook"})})};n.default=r}}]);
+//# sourceMappingURL=983.d0ae16a0.chunk.js.map
