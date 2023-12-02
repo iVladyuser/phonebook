58d58ab0cc75f65a80aed99bfda5fae1e32f33dd
iVladyuser <korol.v09@gmail.com>
2023-12-02 14:25:33 +0100

    added optimization with ROUTES_const

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,12 +4,19 @@ import { Layout } from './Layout/Layout';
 import { Loader } from './Loader/Loader';
 import { useDispatch } from 'react-redux';
 import { refreshThunk } from 'services/fetchAuth';
+import * as ROUTES from 'constants/routes';
 
 const Home = lazy(() => import('pages/HomePage'));
-// const SignOut = lazy(() => import('pages/SignOutPage'));
 const Register = lazy(() => import('pages/RegisterPage'));
-const SignIn = lazy(() => import('pages/LogInPage'));
+const Login = lazy(() => import('pages/LogInPage'));
 const Contacts = lazy(() => import('pages/ContactsPage'));
+
+const appRoutes = [
+  { path: ROUTES.HOME_ROUTE, element: <Home /> },
+  { path: ROUTES.LOGIN_ROUTE, element: <Login /> },
+  { path: ROUTES.REGISTER_ROUTE, element: <Register /> },
+  { path: ROUTES.CONTACTS_ROUTE, element: <Contacts /> },
+];
 
 const App = () => {
   const dispatch = useDispatch();
@@ -17,14 +24,15 @@ const App = () => {
   useEffect(() => {
     dispatch(refreshThunk());
   }, [dispatch]);
+
   return (
     <Layout>
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<SignIn />} />
+          {appRoutes.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Suspense>
